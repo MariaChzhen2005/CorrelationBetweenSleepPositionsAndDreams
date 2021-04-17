5d5f1621c53f31f84540c2f35bd59d5f6984e94f
MariaChzhen2005 <46852765+MariaChzhen2005@users.noreply.github.com>
2021-04-17 21:44:26 +0300

    Add files via upload

---
+++ Gender effects/menPositions.java
@@ -0,0 +1,55 @@
+import org.apache.commons.csv.*;
+import edu.duke.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Arrays;
+
+public class menPositions {
+    //vividity bizarreness happiness scariness romance
+    static ArrayList<Integer> backPoses = new ArrayList<Integer>(Arrays.asList(1,4,5,12,13,14,16,20,22));
+    static ArrayList<Integer> leftPoses = new ArrayList<Integer>(Arrays.asList(3,6,11,21));
+    static ArrayList<Integer> rightPoses = new ArrayList<Integer>(Arrays.asList(9,18,19,23));
+    static ArrayList<Integer> stomachPoses = new ArrayList<Integer>(Arrays.asList(2,7,8,10,15,17,24));
+    public static void menPositions (CSVParser parser) {
+        int back=0, front=0, left=0, right=0;
+        double genderCount=0.0;
+        
+        for (CSVRecord record : parser) {
+            String gender = record.get("gender");
+            String wakePos = record.get("wakePos");
+            int wakePosInt = Integer.parseInt(wakePos);
+            
+            if (gender.equals("Male")) {
+                if (backPoses.contains(wakePosInt)) {
+                    back++;
+                }
+                if (stomachPoses.contains(wakePosInt)) {
+                    front++;
+                }
+                if (leftPoses.contains(wakePosInt)) {
+                    left++;
+                }
+                if (rightPoses.contains(wakePosInt)) {
+                    right++;
+                }
+                genderCount++;
+            }
+            
+        }
+        double backPerc=(back/genderCount)*100, frontPerc=(front/genderCount)*100,
+                leftPerc=(left/genderCount)*100, rightPerc=(right/genderCount)*100;
+        
+        System.out.println("AVERAGE EMOTIONAL PARAMETERS - MEN");
+        System.out.println("The amount of men sleeping on the back is " + backPerc);
+        System.out.println("The amount of men sleeping on the front is " + frontPerc);
+        System.out.println("The amount of men sleeping on the left is " + leftPerc);
+        System.out.println("The amount of men sleeping on the right is " + rightPerc);
+        
+    }
+    
+    public static void tester() {
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser();
+        menPositions(parser);
+    }
+}
